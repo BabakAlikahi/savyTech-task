73306467032834217ccaa2259164d8ec942209e1
babakalikahi72 <babak.alikahi@gmail.com>
2025-10-29 10:21:18 +0330

    set empty components and table for show list of todo

---
+++ src/features/todo/components/empty-todo.tsx
@@ -0,0 +1,26 @@
+import { ClipboardList } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
+
+type Props = {
+  onAction: () => void;
+};
+
+function EmptyTodo({ onAction }: Props) {
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <ClipboardList />
+        </EmptyMedia>
+        <EmptyTitle>No Items Yet</EmptyTitle>
+        <EmptyDescription>You havent created any items yet. Get started by creating your first item.</EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button onClick={onAction}>Create Project</Button>
+      </EmptyContent>
+    </Empty>
+  );
+}
+
+export default EmptyTodo;
--- src/features/todo/components/todo-list.tsx
+++ src/features/todo/components/todo-list.tsx
@@ -1,5 +1,67 @@
+import { Pen, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+import EmptyTodo from "./empty-todo";
+
 function TodoList() {
-  return <div>TodoList</div>;
+  const tableHeader = ["title", "subtitle", "createdAt", "updatedAt", "action"];
+
+  const handleClick = () => {
+    console.log("click");
+  };
+
+  return (
+    <div className="overflow-hidden rounded-md border">
+      <Table>
+        <TableCaption className="pb-4">Todo List</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[50px] text-center">#</TableHead>
+            {tableHeader.map((item, index) => (
+              <TableHead
+                key={index}
+                className="text-center"
+              >
+                {item}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {true ? (
+            <TableRow className="text-center">
+              <TableCell className="font-medium">{"num"}</TableCell>
+              <TableCell>{"item.title"}</TableCell>
+              <TableCell>{"item.subtitle"}</TableCell>
+              <TableCell>{"item.createdAt"}</TableCell>
+              <TableCell>{"item.updatedAt ?? ----"}</TableCell>
+              <TableCell className="flex items-center justify-center gap-2 text-right">
+                <Button size={"icon"}>
+                  <Pen />
+                </Button>
+                <Button
+                  size={"icon"}
+                  variant={"destructive"}
+                >
+                  <Trash />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ) : (
+            <TableRow>
+              <TableCell
+                className="text-center"
+                colSpan={6}
+              >
+                <EmptyTodo onAction={handleClick} />
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
 
 export default TodoList;
